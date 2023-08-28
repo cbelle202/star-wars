7a039b3ecdaef54261431e6aaa7f8939c2204eae
cbelle202 <93744596+cbelle202@users.noreply.github.com>
2023-08-28 00:34:27 -0400

    cleanup

--- app/src/main/java/com/ciarabelle/starwars/components/ListItemComponent.kt
+++ app/src/main/java/com/ciarabelle/starwars/components/ListItemComponent.kt
@@ -1,6 +1,5 @@
 package com.ciarabelle.starwars.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,7 +9,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp

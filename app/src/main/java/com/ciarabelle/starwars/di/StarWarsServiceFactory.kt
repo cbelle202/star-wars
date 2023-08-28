56d5ea1072e264f05110a510e52803f5289b561d
cbelle202 <93744596+cbelle202@users.noreply.github.com>
2023-08-28 08:51:46 -0400

    timeout added via okhttp3 client

--- app/src/main/java/com/ciarabelle/starwars/di/StarWarsServiceFactory.kt
+++ app/src/main/java/com/ciarabelle/starwars/di/StarWarsServiceFactory.kt
@@ -3,8 +3,10 @@ package com.ciarabelle.starwars.di
 import com.ciarabelle.starwars.api.StarWarsService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object StarWarsServiceFactory {
     private const val baseUrl = "https://swapi.dev"
@@ -13,9 +15,14 @@ object StarWarsServiceFactory {
         val moshi = Moshi.Builder()
             .addLast(KotlinJsonAdapterFactory())
             .build()
+        val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
             .build().create(StarWarsService::class.java)
     }
 }

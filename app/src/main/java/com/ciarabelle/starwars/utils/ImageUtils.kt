62cbe92fe0532778636b2a817388233abbab9229
cbelle202 <93744596+cbelle202@users.noreply.github.com>
2023-08-28 00:26:46 -0400

    images update

--- app/src/main/java/com/ciarabelle/starwars/utils/ImageUtils.kt
+++ app/src/main/java/com/ciarabelle/starwars/utils/ImageUtils.kt
@@ -3,7 +3,8 @@ package com.ciarabelle.starwars.utils
 object ImageUtils {
     fun getImage(name: String?): String? {
         return when (name) {
-            "Luke Skywalker" -> "https://icons.iconarchive.com/icons/jonathan-rey/star-wars-characters/256/Luke-Skywalker-03-icon.png"
+            "Luke Skywalker" -> "https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_7ffe21c7.jpeg"
+            "Darth Vader" -> "https://imageio.forbes.com/specials-images/imageserve/6090f7f251c9c6c605e612fc/Darth-Vader/0x0.jpg"
             else -> null
         }
     }
